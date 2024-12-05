41ca053f00379dbd7ec0d1bd73f67a254afb6729
Daeeun Kang <daeeun123@daum.net>
2024-12-05 20:09:37 +0900

    feat:useQuery기본적인실행순서

--- tanstack-query-app/src/App.jsx
+++ tanstack-query-app/src/App.jsx
@@ -1,84 +1,19 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
-import { useState } from "react";
+// src > App.jsx
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Empty from "./pages/Empty";
 
-const App = () => {
-  const queryClient = useQueryClient();
-  const [todoItem, setTodoItem] = useState("");
-
-  const fetchTodos = async () => {
-    const response = await axios.get("http://localhost:4000/todos");
-    return response.data;
-  };
-
-  const addTodo = async (newTodo) => {
-    await axios.post("http://localhost:4000/todos", newTodo);
-  };
-
-  const {
-    data: todos,
-    isPending,
-    isError,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
-
-  const { mutate } = useMutation({
-    mutationFn: addTodo,
-    onSuccess: () => {
-      // alert("데이터 삽입이 성공했습니다.");
-      // 데이터를 캐싱하는 기준은 쿼리키가 된다. ["todos"] 즉, 이 부분이 바로 리렌더링해주는 부분
-      queryClient.invalidateQueries(["todos"]);
-    },
-  });
-
-  if (isPending) {
-    return <div>로딩중입니다...</div>;
-  }
-
-  if (isError) {
-    return <div>데이터 조회 중 오류가 발생했습니다.</div>;
-  }
-
+function App() {
   return (
-    <div>
-      <h3>Tanstack Query</h3>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const newTodoObj = { title: todoItem, isDone: false };
-          // useMutation 로직 필요
-          mutate(newTodoObj);
-        }}
-      >
-        <input
-          type="text"
-          value={todoItem}
-          onChange={(e) => setTodoItem(e.target.value)}
-        />
-        <button>추가</button>
-      </form>
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li
-              key={todo.id}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                gap: "10px",
-                backgroundColor: "aliceblue",
-              }}
-            >
-              <h4>{todo.title}</h4>
-              <p>{todo.isDone ? "Done" : "Not Done"}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- tanstack-query-app/src/main.jsx
+++ tanstack-query-app/src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+// src > main.jsx
+
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <App />
+  </QueryClientProvider>
 );
